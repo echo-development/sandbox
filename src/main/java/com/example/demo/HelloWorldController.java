808d604f1864ba8bdfe9f905f7d5969229cf34c8
Martin Lund <privatmartinlund@gmail.com>
2019-09-15 13:21:27 +0200

    Added CORSFilter and changed port to use AWS 5000

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello-world")
+    @GetMapping(value = "/hello-world")
     @ResponseBody
     public String sayHello() {
         return "Hello world";
